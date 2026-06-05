cfb3c98f9975da684728ea6b98ac1bf668ce523d
Yang-wen-12 <15675438374@163.com>
2026-06-05 04:03:43 -0400

    feat:加强项目练习

---
+++ industrial_master/build/CMakeFiles/sensor_master.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yw/workpractice/industrial_master/core/linklist.c" "CMakeFiles/sensor_master.dir/core/linklist.c.o" "gcc" "CMakeFiles/sensor_master.dir/core/linklist.c.o.d"
+  "/home/yw/workpractice/industrial_master/encrypt/crypto.c" "CMakeFiles/sensor_master.dir/encrypt/crypto.c.o" "gcc" "CMakeFiles/sensor_master.dir/encrypt/crypto.c.o.d"
+  "/home/yw/workpractice/industrial_master/io/fileio.c" "CMakeFiles/sensor_master.dir/io/fileio.c.o" "gcc" "CMakeFiles/sensor_master.dir/io/fileio.c.o.d"
+  "/home/yw/workpractice/industrial_master/log/logger.c" "CMakeFiles/sensor_master.dir/log/logger.c.o" "gcc" "CMakeFiles/sensor_master.dir/log/logger.c.o.d"
+  "/home/yw/workpractice/industrial_master/src/main.c" "CMakeFiles/sensor_master.dir/src/main.c.o" "gcc" "CMakeFiles/sensor_master.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ industrial_master/build/CMakeFiles/sensor_master.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor_master.dir/core/linklist.c.o"
+  "CMakeFiles/sensor_master.dir/core/linklist.c.o.d"
+  "CMakeFiles/sensor_master.dir/encrypt/crypto.c.o"
+  "CMakeFiles/sensor_master.dir/encrypt/crypto.c.o.d"
+  "CMakeFiles/sensor_master.dir/io/fileio.c.o"
+  "CMakeFiles/sensor_master.dir/io/fileio.c.o.d"
+  "CMakeFiles/sensor_master.dir/log/logger.c.o"
+  "CMakeFiles/sensor_master.dir/log/logger.c.o.d"
+  "CMakeFiles/sensor_master.dir/src/main.c.o"
+  "CMakeFiles/sensor_master.dir/src/main.c.o.d"
+  "sensor_master"
+  "sensor_master.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sensor_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
